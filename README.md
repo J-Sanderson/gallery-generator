0780ac0daf1b5685fec78b19333a18617427234a
J Sanderson <ju.a.sanderson@gmail.com>
2020-09-28 23:28:57 +0100

    pass images per page as optional argument

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 WIP - generate image galleries in static HTML. Populate images.json with the required files.
 
 Run: `npm install`, then `node gen.js`
+
+## Flags
+
+`-n (number)` - specify images per page. Defaults to 10 if not given
--- gen.js
+++ gen.js
@@ -2,11 +2,14 @@
 const fs = require("fs");
 const { performance } = require("perf_hooks");
 const Jimp = require("jimp");
+const parseArgs = require("minimist");
 
 let t0 = performance.now();
 
+let args = parseArgs(process.argv.slice(2));
+
 const thumbSize = 200;
-const imagesToPage = 10;
+const imagesToPage = args.n || 10;
 
 console.log("starting generator...");
 
